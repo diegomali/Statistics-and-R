6ad6510209403f0b1602038d735db2e05cd5b099
Diego Martins <diego.mali@hotmail.com>
2016-11-28 15:34:13 -0200

    WIP week 2

---
+++ project one week 2/project one/CLT and t-distribution in Practice Exercises.R
@@ -0,0 +1,43 @@
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv"
+if(!file.exists("femaleMiceWeights.csv")) download(url,destfile=filename)
+dat <- read.csv(filename)
+
+#CLT and t-distribution in Practice Exercises #1
+
+set.seed(1)
+n <- 100
+numberOfSides <- 6
+p=1/numberOfSides
+zs <- replicate(10000, {
+  x=sample(1:numberOfSides, n, replace=TRUE)
+  (mean(x==numberOfSides) - p) / sqrt(p*(1-p)/n)
+})
+mean( abs(zs) > 2)
+#CLT and t-distribution in Practice Exercises #2
+ps <- c(0.5,0.5,0.01,0.01)
+ns <- c(5,30,30,100)
+library(rafalib)
+mypar(4,2)
+for(i in 1:4){
+  p <- ps[i]
+  sides <- 1/p
+  n <- ns[i]
+  zs <- replicate(10000,{
+    x <- sample(1:sides,n,replace=TRUE)
+    (mean(x==1) - p) / sqrt(p*(1-p)/n)
+  }) 
+  hist(zs,nclass=7)
+  qqnorm(zs)
+  abline(0,1)
+}
+#CLT and t-distribution in Practice Exercises #3
+library(dplyr)
+X <- filter(dat, Diet=="chow") %>% select(Bodyweight) %>% unlist
+Y <- filter(dat, Diet=="hf") %>% select(Bodyweight) %>% unlist
+mean(X)
+#CLT and t-distribution in Practice Exercises #4
+#aproximate by SE
+#read more about 
+4
